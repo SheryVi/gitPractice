7eedc9e705b4c436df2babb69552f40af9101868
Victoria <v9858582209@gmail.com>
2022-11-29 18:11:48 +0300

    I have updated some code

--- src/git/batch12.java
+++ src/git/batch12.java
@@ -3,6 +3,7 @@ package git;
 public class batch12 {
     public static void main(String[] args) {
         System.out.println("we we are were");
+        System.out.println("we we are were");
 
 
     }
