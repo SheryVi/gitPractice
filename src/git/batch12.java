2675dfdd90ac302aadd49552f549373b6a0e2ab6
Victoria <v9858582209@gmail.com>
2022-11-29 18:28:29 +0300

    the commit that was easy

--- src/git/batch12.java
+++ src/git/batch12.java
@@ -4,6 +4,7 @@ public class batch12 {
     public static void main(String[] args) {
         System.out.println("we we are were");
         System.out.println("we we are were");
+        System.out.println("some changes");
 
 
     }
